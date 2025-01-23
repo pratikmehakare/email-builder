9eadde557dfa023a1bfce48131104744e467a56c
pratikmehakare <pratikmehakare8.com>
2025-01-23 15:54:03 +0530

    Update Logo

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'; 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config();  
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    // Make sure MONGO_URI is loaded correctly
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
+    console.error(error);
     process.exit(1); // Exit process with failure
   }
 };
 
-export default connectDB;  
+export default connectDB;
